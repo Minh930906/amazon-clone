059726c25e8c2b0b1669007d6509d7b0cde0f5c6
minh9 <minh930906@gmail.com>
2020-09-09 21:10:13 +0200

    amazone added header,checkout,home,product and subtotal component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAehbW4lUbxQz_VIlPOPkfOX94dvVbTm04",
+  authDomain: "clone-8a081.firebaseapp.com",
+  databaseURL: "https://clone-8a081.firebaseio.com",
+  projectId: "clone-8a081",
+  storageBucket: "clone-8a081.appspot.com",
+  messagingSenderId: "1027484025908",
+  appId: "1:1027484025908:web:aec8fcbd57ed4611b7b661",
+  measurementId: "G-4L28NFK13Q",
+};
